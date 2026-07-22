ace2786ba3902fa9b6a12382e3f7a94ace9e6232
Sameer Gautam <sameergautam024@gmail.com>
2026-07-22 16:20:32 +0530

    implement signout feature

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,3 +83,13 @@ export const google = async (req, res, next) => {
         next(error);
     }
 };
+
+export const signout = async (req, res, next) => {
+    try {
+        res.clearCookie('access_token');
+        // console.log("User logged out successfully");
+        res.status(200).json('User has been logged out!');
+    } catch (error) {
+        next(error);
+    }
+};
--- client/src/pages/Customer/Profile.jsx
+++ client/src/pages/Customer/Profile.jsx
@@ -10,6 +10,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
   // signOutUserStart,
 } from '../../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
@@ -143,6 +144,21 @@ const Profile = () => {
       dispatch(deleteUserFailure(error.message));
     }
   }
+  const handleSignOut = async() => {
+    try{
+       dispatch(signOutUserStart())
+       const res = await fetch('/api/auth/signout');
+       const data = await res.json();
+       if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+       }
+       dispatch(deleteUserSuccess(data));
+    } catch (error){
+        dispatch(deleteUserFailure(error.message));
+    }
+  }
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -215,8 +231,10 @@ const Profile = () => {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
         <p className='text-red-700 mt-5'>{error ? error : ''}</p>
         <p className='text-green-700 mt-5'>
           {updateSuccess ? 'User is updated successfully' : ''}</p>
